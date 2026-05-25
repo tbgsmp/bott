b23a15b6a85356fb4203e7347264644736df935c
goldprodude <goldprodude@gmail.com>
2026-05-25 10:57:01 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,66 @@
+const mineflayer = require('mineflayer')
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('AFK Bot Running')
+})
+
+app.listen(process.env.PORT || 3000)
+
+function createBot() {
+
+  const bot = mineflayer.createBot({
+    host: 'tbg.freezehost.com',
+    port: 25565,
+    username: 'ServerTBG',
+    auth: 'offline',
+    version: '1.20.4'
+  })
+
+  bot.on('spawn', () => {
+    console.log('Bot joined server!')
+
+    // AuthMe login
+    setTimeout(() => {
+      bot.chat('/login YOUR_PASSWORD')
+    }, 3000)
+
+    // Anti AFK
+    setInterval(() => {
+
+      bot.setControlState('jump', true)
+
+      setTimeout(() => {
+        bot.setControlState('jump', false)
+      }, 1000)
+
+      const directions = ['forward', 'back', 'left', 'right']
+
+      const dir = directions[Math.floor(Math.random() * directions.length)]
+
+      bot.setControlState(dir, true)
+
+      setTimeout(() => {
+        bot.setControlState(dir, false)
+      }, 2000)
+
+      bot.look(
+        Math.random() * Math.PI * 2,
+        Math.random() * Math.PI / 2,
+        true
+      )
+
+    }, 15000)
+  })
+
+  bot.on('end', () => {
+    console.log('Disconnected. Reconnecting in 10 seconds...')
+    setTimeout(createBot, 10000)
+  })
+
+  bot.on('error', err => console.log(err))
+}
+
+createBot()
